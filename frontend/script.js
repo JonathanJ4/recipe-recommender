afde92b4e1f04cad9f6e3bc1d4b547a1ab501a9f
Jonathan Thomas Varughese <jonathanthomasv6@gmail.com>
2025-05-10 14:18:16 -0400

    Fixed the card display section

--- frontend/script.js
+++ frontend/script.js
@@ -16,3 +16,21 @@ const recipes=[
   }
 ]
 
+const trendingsection = document.querySelector('.trending')
+
+const cardContainer = document.createElement('div')
+cardContainer.classList.add('trending-card')
+
+recipes.forEach(recipe => {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  card.innerHTML = `
+    <img src="${recipe.image}" alt="${recipe.title}">
+    <h4>${recipe.title}</h4>
+    <p>${recipe.description}</p>
+  `;
+
+  cardContainer.appendChild(card);
+});
+trendingSection.appendChild(cardContainer);
